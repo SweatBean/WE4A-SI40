7adb6c7d2e7b3ee423c87b1ef39a4ac4a12cbe09
Rick_Sanchez <clementlebert34@gmail.com>
2025-04-07 21:00:40 +0200

    Add files via upload

---
+++ inscription.js
@@ -0,0 +1,28 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const formConnexion = document.getElementById("form_connexion");
+
+    if (formConnexion) {
+        formConnexion.addEventListener("submit", function (event) {
+            event.preventDefault(); // Empêche le rechargement
+
+            const nom = document.getElementById("utilisateur").value;
+            const motDePasse = document.getElementById("mdp").value;
+            const motDePasseConfirm = document.getElementById("mdp_confirm").value;
+            const messageErreur = document.getElementById("erreur_message");
+
+            console.log("Nom d'utilisateur :", nom);
+            console.log("Mot de passe :", motDePasse);
+            console.log("Confirmation :", motDePasseConfirm);
+
+            //todo verifier nom utilisateur pas déjà utiliser
+
+            // Vérification des mots de passe
+            if (motDePasse !== motDePasseConfirm) {
+                messageErreur.textContent = "Les mots de passe ne correspondent pas.";
+                return;
+            }
+
+            window.location.href = "accueil.html"; // ou redirection si tu veux
+        });
+    }
+});
